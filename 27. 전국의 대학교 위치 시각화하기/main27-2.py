e71ec4fa4cae1d64d07be445a573cf5ba6e6c321
donghoon <ldh8206@gmail.com>
2022-10-23 22:48:45 +0900

    key제외

--- 27. 전국의 대학교 위치 시각화하기/main27-2.py
+++ 27. 전국의 대학교 위치 시각화하기/main27-2.py
@@ -1,12 +1,14 @@
 import requests
 
+import keyStore
+
 url = 'http://api.vworld.kr/req/address?'
 params = 'service=address&request=getcoord&version=2.0&crs=epsg:4326&refine=true&simple=false&format=json&type='
 road_type = 'ROAD'
 road_type2 = 'PARCEL'
 address = '&address='
 keys = '&key='
-primary_key = 'B441B44D-388E-3ED9-A964-68844EAF7946'
+primary_key = keyStore.main27_primary_key
 
 def request_geo(road):
     page = requests.get(url+params+road_type+address+road+keys+primary_key)
