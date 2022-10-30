ce175d48461398dedec94ebfe13b3c1564345d67
donghoon <ldh8206@gmail.com>
2022-10-30 12:49:27 +0900

    33, 34, 35

---
+++ 33. 계산기 만들기/main33-1.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+ui_path = "계산기.ui"
+form_class = uic.loadUiType(ui_path)[0]
+
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+if __name__ == "__main__" :
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
